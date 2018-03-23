a53de5f73113c01d7062fced744aa007275ce467
Filip Grudziński <d1nsiak@gmail.com>
2018-03-23 21:33:22 +0100

    First Swift App

--- dogApp/ViewController.swift
+++ dogApp/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        if let age = textField.text {
+            
+            if let ageAsNumber = Int(age) {
+                
+                if (ageAsNumber <= 0) {
+                    
+                    label.text = "Your dog never born!"
+                }
+                else{
+                    let dogYears = 7 * ageAsNumber
+                
+                    label.text = "Your dog is " + String(dogYears) + " dog years!"
+                    }
+                }
+                
+            }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
